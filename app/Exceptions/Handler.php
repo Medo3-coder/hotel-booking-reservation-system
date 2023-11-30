2bfe81f93d2951c0cc46a8ab3fcf1ec241855f83
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2023-11-30 14:21:32 +0200

    Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,16 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ResponseTrait;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
-class Handler extends ExceptionHandler
-{
+class Handler extends ExceptionHandler {
+
+    use ResponseTrait;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -21,10 +26,26 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    public function report(Throwable $exception): void {
+        parent::report($exception);
+    }
+
+    public function render($request, Throwable $exception) {
+        if ($request->is('api/*')) {
+            if ($exception instanceof ModelNotFoundException) {
+                $msg = "Model Not Found";
+            }
+            if ($exception instanceof NotFoundHttpException) {
+                $msg = "Not Found HTTP";
+            }
+            if ($exception instanceof AuthenticationException) {
+                return $this->unauthenticatedReturn();
+            }
+
+            return $this->response('exception', $message ?? $exception->getMessage(),
+                ['line' => $exception->getLine(), 'file' => $exception->getFile()]);
+        }
+
+        return parent::render($request, $exception);
     }
 }
---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Traits;
+
+trait ResponseTrait {
+    /**
+     * keys : success, fail, needActive, waitingApprove, unauthenticated, blocked, exception
+     */
+
+    public function response($key, $message, $data = [], $anotherKey = [], $page = []) {
+
+        $allResponse['key']     = (string) $key;
+        $allResponse['message'] = (string) $message;
+
+        # unread notifications count if request ask
+        if ($key == 'success' && request()->has('count_notifications')) {
+            $count = 0;
+            if (auth()->check()) {
+                $count = auth()->user()->notifications()->unread()->count();
+            }
+
+            $allResponse['count_notifications'] = $count;
+        }
+
+        # additional data
+        if (!empty($anotherKey)) {
+            foreach ($anotherKey as $otherKeys => $value) {
+                $allResponse[$otherKeys] = $value;
+            }
+        }
+
+        # res data
+        if ($data = ![] && (in_array($key, ['success', 'needActive', 'exception']))) {
+            $allResponse['data'] = $data;
+        }
+
+    }
+
+    public function unauthenticatedReturn() {
+        return $this->response('unauthorized', 'Please login again');
+    }
+
+}
